ff5ab65ee508f083c257b7a683f96c1a29b88b97
Steven Pelliet <steven@pelliet.fr>
2018-01-15 10:15:50 +0000

    Upload New File

---
+++ view/addProduct.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * User: Pelliesi
+ * Date: 15/01/2018
+ * Time: 11:10
+ */
+
+?>
+<div>
+<h1>Ajout d'un produit</h1>
+<form method="post" role="add" action="<?php echo $_GET['host']."/index.php?action=addProduct" ?>" id="addProduct">
+
+
+    <button id="addbtn" type="submit">Valider</button>
+    <button id="cancbtn" type="submit">Annuler</button>
+</form>
+<script src="./assets/js/advSearch_method.js"></script>
+</div>
